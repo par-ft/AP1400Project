47b4d1882d619d28aaef4adcb40db8d93494a301
par-ft <100035078+par-ft@users.noreply.github.com>
2022-07-01 12:09:24 +0430

    add Qprogressbar and edit some codes

--- proj.py
+++ proj.py
@@ -1,9 +1,12 @@
 import os
+from time import time
 import PyQt5
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import numpy as np
+
+
 
 
 Form = uic.loadUiType(os.path.join(os.getcwd(), "Form.ui"))[0]
@@ -14,11 +17,27 @@ class IntroWindow(QMainWindow, Form):
         self.setupUi(self)
 
         self.hint.clicked.connect(self.sayhint)
-        self.quit.clicked.connect(self.quit)
+        self.quit.clicked.connect(lambda: app.quit())
+        self.text_time.setText(f"{time_ns()/1000000}ms")
+        for  i in range (81):
+            self.text_xy.setText()
+            #تو کدت اونجایی که خونه ها تعریف میشن چی باشن اینو ست تکست کن کن
+        self.text_xy.textChanged.connect(self.textchange)
+        self.progressBar.setRange (maximum,minimum)
+        self.progressBar.valueChanged()
+        #برای اینکه نشون بده چند درصدش کامل شده اینجا درصد میدی نشون میده
+
+
+
 
     def sayhint(self):
        #hint code here
        # self.text_xy.setText(f"{}")
+        pass
+    def textchange(self):
+        #اینجا باید یه مشت ایف بزنی چک کنه ببینه درسته یا نه
+        pass
+    #کد رو نوشتی passهارو بردار 
 
 
 if __name__ == "__main__":
